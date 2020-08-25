226c9f93dbbc244d63345aa2631b1023f2f4e2bc
Epeli Ratoto <eratoto@yahoo.com>
2020-08-25 04:24:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@ var playlist = {
   artistName: "I am Free"
 }
 
-function updatePlayList (playlist, artistName, songtitle) {
-  playlist.artistName = songtitle
+function updatePlayList (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
   return playlist
 }
 updatePlayList(playlist, "Woo", "Wan")
